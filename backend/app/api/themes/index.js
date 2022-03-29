7b316c0ba1d727934ee683bf8b35a5c139690489
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-03-29 11:13:52 +0200

    Update back

---
+++ backend/app/api/themes/index.js
@@ -0,0 +1,55 @@
+const { Router } = require('express')
+
+const { Theme } = require('../../models')
+
+const router = new Router()
+
+router.get('/', (req, res) => {
+  try {
+    res.status(200).json(Theme.get())
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+router.get('/:themeId', (req, res) => {
+  try {
+    res.status(200).json(Theme.getById(req.params.themeId))
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.post('/', (req, res) => {
+  try {
+    const user = Theme.create({ ...req.body })
+    res.status(201).json(user)
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err.extra)
+    } else {
+      res.status(500).json(err)
+    }
+  }
+})
+
+router.delete('/:themeId', (req, res) => {
+  try {
+    res.status(200).json(Theme.delete(req.params.themeId))
+    console.log(req.params.themeId)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:themeId', (req, res) => {
+  try {
+    res.status(200).json(Theme.update(req.params.themeId, req.body))
+    console.log(req.body)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
