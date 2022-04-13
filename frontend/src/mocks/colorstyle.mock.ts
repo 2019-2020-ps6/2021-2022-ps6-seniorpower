d3c572875ba00e8164d1715312b5f5f19261fed7
baidouri <alibaidouri7@gmail.com>
2022-04-13 12:49:24 +0000

    play-quiz form color edit #2

--- frontend/src/mocks/colorstyle.mock.ts
+++ frontend/src/mocks/colorstyle.mock.ts
@@ -4,7 +4,10 @@ export const DEFAULT_COLOR:ColorStyle = {
   font: {
     colorLoupe: "rgb(230,255,255)",
     colorTrue: "#92C746", //ou #74A529
-    colorFalse: "#ea9999" //ou #D12F11 ou #FF6119
+    colorFalse: "#ea9999" ,//ou #D12F11 ou #FF6119
+    colorScript: "#c8cbee",
+    colorNext:"#ff8a66",
+    colorAfficher:"#50DBB4",
   },
 }
 
@@ -12,7 +15,10 @@ export const PROTA_COLOR:ColorStyle = {
   font: {
     colorLoupe: "lightgreen",
     colorTrue: "#ff9900",
-    colorFalse: "#8eaff6"
+    colorFalse: "#8eaff6",
+    colorScript:"#c8cbee",
+    colorNext:"#ff8a66",
+    colorAfficher:"#50DBB4"
   },
 }
 
@@ -20,7 +26,10 @@ export const TRITA_COLOR:ColorStyle = {
   font: {
     colorLoupe: "rgb(230,255,255)",
     colorTrue: "#7fd3ec",
-    colorFalse: "#e55f5f"
+    colorFalse: "#e55f5f",
+    colorScript:"#c8cbee",
+    colorNext:"#f1d0ed",
+    colorAfficher:"#50DBB4"
   },
 }
 
@@ -28,6 +37,9 @@ export const DEUTE_COLOR:ColorStyle = {
   font: {
     colorLoupe: "rgb(255,224,192)",
     colorTrue: "#ff9900",
-    colorFalse: "#8eaff6"
+    colorFalse: "#8eaff6",
+    colorScript:"#c8cbee",
+    colorNext:"#ff8a66",
+    colorAfficher:"#50DBB4"
   },
 }
--- frontend/src/models/colorstyle.model.ts
+++ frontend/src/models/colorstyle.model.ts
@@ -1,8 +1,11 @@
 export interface ColorStyle { //TODO Modele definitif a definir
   font: {
+    colorScript: string;
     colorLoupe: string;
     colorTrue?: string;
     colorFalse?: string;
+    colorNext:string;
+    colorAfficher:string
   };
 
 }
