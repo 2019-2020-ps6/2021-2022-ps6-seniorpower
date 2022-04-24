253ca2df86aaea84e56bf728a7a6e279d35ad565
Lionel AREL <lionel.arel@outlook.fr>
2022-04-24 21:56:00 +0200

    aside bar v2

--- frontend/src/app/aside/aside.component.ts
+++ frontend/src/app/aside/aside.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit() {
   }
