e869146ec751bd83b26fa2275a9c8ba7aec4f6e1
baidouri <alibaidouri7@gmail.com>
2022-04-24 12:48:02 +0200

    user-form inscription modification

--- frontend/src/app/users/user-form/user-form.component.ts
+++ frontend/src/app/users/user-form/user-form.component.ts
@@ -3,6 +3,12 @@ import { FormGroup } from '@angular/forms';
 import { FormBuilder, Validators} from "@angular/forms";
 import {User} from "../../../models/user.model";
 import {UserService} from "../../../services/user.service";
+import {CLASSIC_Format, DMLA_FORMAT, GLAUCOME_FORMAT} from "../../../mocks/formatting.mock";
+import {Formatting} from "../../../models/formatting.model";
+import {FormattingService} from "../../../services/formatting.service";
+import {ColorService} from "../../../services/color.service";
+import {ColorStyle} from "../../../models/colorstyle.model";
+import {DEFAULT_COLOR, DEUTE_COLOR, PROTA_COLOR, TRITA_COLOR} from "../../../mocks/colorstyle.mock";
 
 @Component({
   selector: 'app-user-form',
@@ -14,7 +20,15 @@ export class UserFormComponent implements OnInit{
 
   public userForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder, public userService: UserService) {
+  formatting:Formatting = CLASSIC_Format;
+  colorStyle:ColorStyle = DEFAULT_COLOR;
+  colorList:String[] = ["Aucun","Protanopie","Tritanopie","Deutéranopie"];
+  illnessList:String[] = ["Aucune","DMLA","Glaucome"];
+  currentColor:string;
+  currentIllness:string;
+
+
+  constructor(public formBuilder: FormBuilder, public userService: UserService,public formattingService:FormattingService,public colorService: ColorService) {
     this.userForm = this.formBuilder.group({
       name: [null, Validators.required],
       nom: [null, Validators.required],
@@ -23,6 +37,15 @@ export class UserFormComponent implements OnInit{
       daltonisme: [null, Validators.required],
       password: [null, Validators.required],
       id:['']
+    });
+    this.formattingService.getFormatting().subscribe((format)=> {
+      this.formatting = format;
+    });
+    this.colorService.getColorStyle().subscribe((color) => {
+      this.colorStyle = color;
+    });
+    this.formattingService.getCurrentIllness().subscribe((illness)=>{
+      this.currentIllness = illness;
     });
   }
 
@@ -37,5 +60,97 @@ export class UserFormComponent implements OnInit{
     console.log('Add User: ', userToCreate);
     this.userService.addUser(userToCreate);
   }
+  getAllIllness(){
+    return this.illnessList;
+  }
+  applyIllnessChange(){
+    const illness: string = (document.getElementById('illness') as HTMLInputElement).value;
+    switch (illness){
+      case "Aucune":{
+        this.changeFormatClassic();
+        break;
+      }
+      case "DMLA":{
+        this.changeFormatDMLA();
+        break;
+      }
+      case "Glaucome":{
+        this.changeFormatGlaucome();
+        break;
+      }
+    }
+  }
+  changeFormatClassic(){
+    this.formatting = CLASSIC_Format;
+    this.formattingService.formattingUpdate(this.formatting);
+    this.formattingService.changeIllness("Aucune");
+  }
+
+  changeFormatDMLA(){
+    this.formatting = DMLA_FORMAT;
+    this.formattingService.formattingUpdate(this.formatting);
+    this.formattingService.changeIllness("DMLA");
+
+  }
+
+  changeFormatGlaucome(){
+    this.formatting = GLAUCOME_FORMAT;
+    this.formattingService.formattingUpdate(this.formatting);
+    this.formattingService.changeIllness("Glaucome");
+
+  }
+  changeColorDefault(){
+    this.colorStyle = DEFAULT_COLOR;
+    this.colorService.colorUpdate(this.colorStyle);
+    this.colorService.currentColorUpdate("Aucun");
+  }
+
+  changeColorProta(){
+    this.colorStyle = PROTA_COLOR;
+    this.colorService.colorUpdate(this.colorStyle);
+    this.colorService.currentColorUpdate("Protanopie");
+
+  }
+
+  changeColorTrita(){
+    this.colorStyle = TRITA_COLOR;
+    this.colorService.colorUpdate(this.colorStyle);
+    this.colorService.currentColorUpdate("Tritanopie");
+
+  }
+
+  changeColorDeute(){
+    this.colorStyle = DEUTE_COLOR;
+    this.colorService.colorUpdate(this.colorStyle);
+    this.colorService.currentColorUpdate("Deutéranopie");
+
+  }
+  getAllColors(){
+    return this.colorList;
+  }
+
+  applyColorChange(){
+    const color: string = (document.getElementById('color') as HTMLInputElement).value;
+    switch (color){
+      case "Aucun":{
+        this.changeColorDefault();
+        break;
+      }
+      case "Protanopie":{
+        this.changeColorProta();
+        break;
+      }
+      case "Tritanopie":{
+        this.changeColorTrita();
+        break;
+      }
+      case "Deutéranopie":{
+        this.changeColorDeute();
+        break;
+      }
+    }
+  }
+
+
 }
 
